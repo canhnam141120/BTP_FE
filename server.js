8a17bcf5d42b8821de3b0502e1e8617dbbef6bdd
namnc <namnche141707@fpt.edu.vn>
2022-12-05 00:03:54 +0700

    Test

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express')
 const serveStatic = require('serve-static')
 const path = require('path')
 const app = express()
-app.use('https://tram-sach.herokuapp.com/',serveStatic(path.join(__dirname,'/dist')))
+app.use('/',serveStatic(path.join(__dirname,'/dist')))
+
+app.get(/.*/, function (req, res){
+    res.sendFile(path.join(__dirname, '/dist/index.html'))
+})
+
 const port = process.env.PORT || 8080
 app.listen(port)
